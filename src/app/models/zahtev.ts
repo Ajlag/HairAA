46525d7505c7657ccd127baa326ff80a55993d5f
unknown <ajlagudzevic1100@gmail.com>
2021-08-26 23:08:24 +0200

    Zavrseni zahtevi

--- src/app/models/zahtev.ts
+++ src/app/models/zahtev.ts
@@ -3,15 +3,16 @@ import { Time } from "@angular/common";
 export class Zahtev {
     idZahteva: number;
     vrsta: string;
-    datum: string;
+    datum:string;
     vreme: string;
     status: string;
 
-    constructor(idZahteva:number,vrsta:string,datum: string,vreme:string, status: string) {
+    constructor(idZahteva:number,vrsta:string, datum: string,vreme:string, status:string) {
         this.idZahteva =idZahteva;
         this.vrsta=vrsta;
+        this.datum = datum;
         this.vreme=vreme;
-        this.datum=datum;
         this.status=status;
+        
         }
 }
