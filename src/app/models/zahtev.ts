8f6c247f4930ac5def7076dc028c2c655d5c81a9
unknown <ajlagudzevic1100@gmail.com>
2021-08-26 22:19:14 +0200

    Slanje zahteva sa greskom u backend

--- src/app/models/zahtev.ts
+++ src/app/models/zahtev.ts
@@ -3,14 +3,15 @@ import { Time } from "@angular/common";
 export class Zahtev {
     idZahteva: number;
     vrsta: string;
-    datum: Date;
+    datum: string;
     vreme: string;
     status: string;
 
-    constructor(idZahteva:number,vrsta:string,datum: Date,vreme:string,status:string) {
+    constructor(idZahteva:number,vrsta:string,datum: string,vreme:string, status: string) {
         this.idZahteva =idZahteva;
         this.vrsta=vrsta;
         this.vreme=vreme;
+        this.datum=datum;
         this.status=status;
         }
 }
--- src/app/usluga/usluga.component.ts
+++ src/app/usluga/usluga.component.ts
@@ -7,7 +7,7 @@ import { odobriZahtevURL, zahtevURL } from '../config/api';
 import { Usluge } from '../models/usluge';
 import { Zahtev } from '../models/zahtev';
 import { UslugeService } from '../services/usluge.service';
-import { Time } from '@angular/common';
+
 @Component({
   selector: 'app-usluga',
   templateUrl: './usluga.component.html',
@@ -20,13 +20,12 @@ export class UslugaComponent implements OnInit {
 
   zahtev: Zahtev;
   zahtevi: Zahtev[]=[];
-  createForm: FormGroup;
+
   editForm: FormGroup;
   selectedOne: Usluge = null;
+  editLoad: boolean = false;
 
-  createLoad:boolean = false;
-  subC =  false;
-
+  subE = false;
   model;
 
   title="addBootstrap";
@@ -34,7 +33,6 @@ export class UslugaComponent implements OnInit {
 
   ngOnInit(): void {
     this.getUsluge();
-    this.initCreateForm();
     this.initEditForm();
   }
 
@@ -48,21 +46,14 @@ export class UslugaComponent implements OnInit {
     this.us.getZahtevi().subscribe((zah: Zahtev[]) => {
       this.zahtevi = zah})
   }
-initCreateForm() {
-  this.createForm = this.fb.group({
-     vrstaE: ['', Validators.required],
-     cenaE: ['', Validators.required],
-     datumE:['',Validators.required],
-    vremeE:['',Validators.required]
-  })
-}
 
 
 initEditForm() {
   this.editForm = this.fb.group({
-    idUslugeE: ['', Validators.required],
-    vrstaE: ['', Validators.required],
-    cenaE: ['', Validators.required]
+      vrstaE: ['', Validators.required],
+     cenaE: ['', Validators.required],
+     datumE:['',Validators.required],
+    vremeE:['',Validators.required]
   })
 }
 
@@ -78,33 +69,49 @@ selectOne(usluga: Usluge) {
 }
 
 
-get newsP() {
-  return this.createForm.controls;
-}
-
 get editsP() {
   return this.editForm.controls;
 }
 
 
-onCreate() {
+
+onUpdate() {
   
-      this.subC = true;
-      this.createLoad = true;
-      if(this.createForm.invalid) {
+  this.subE = true;
+      this.editLoad = true;
+      if(this.editForm.invalid) {
         alert("Nevalidna forma.");
-        this.createLoad = false;
+        this.editLoad = false;
         return;
       }
-  let zahtev = new Zahtev(0, this.newsP.vrsta.value,this.newsP.datum.value, this.newsP.vreme.value, this.newsP.status.value);
-    console.log(zahtev);
+    let product = new Zahtev(0, this.editsP.vrstaE.value, this.editsP.datumE.value, this.editsP.vremeE.value,'cekanje');
 
-    this.us.createZahtev(zahtev).subscribe(response => {alert(JSON.stringify("Zahtev poslat!"))
+    this.us.createZahtev(product).subscribe(response => {alert(JSON.stringify("Zahtev poslat!"))
     console.log(response)
     this.getZahtevi();
   }, err => console.log(err)
     );
-    this.createLoad=false;
+    this.editLoad=false;
 }
+
+// onCreate() {
+  
+//       this.subC = true;
+//       this.createLoad = true;
+//       if(this.createForm.invalid) {
+//         alert("Nevalidna forma.");
+//         this.createLoad = false;
+//         return;
+//       }
+//   let zahtev = new Zahtev(0, this.newsP.vrsta.value,this.newsP.datum.value, this.newsP.vreme.value, this.newsP.status.value);
+//     console.log(zahtev);
+
+//     this.us.createZahtev(zahtev).subscribe(response => {alert(JSON.stringify("Zahtev poslat!"))
+//     console.log(response)
+//     this.getZahtevi();
+//   }, err => console.log(err)
+//     );
+//     this.createLoad=false;
+// }
 
 }
