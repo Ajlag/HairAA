290ff874a6aeac89a4f4d08f51bce9515041c1ab
unknown <ajlagudzevic1100@gmail.com>
2021-08-28 23:49:47 +0200

    Get email in staff

--- src/app/moji-zahtevi/moji-zahtevi.component.ts
+++ src/app/moji-zahtevi/moji-zahtevi.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from '../models/user';
 import { Zahtev } from '../models/zahtev';
+import { UserService } from '../services/user.service';
 import { UslugeService } from '../services/usluge.service';
 
 @Component({
@@ -12,13 +13,14 @@ import { UslugeService } from '../services/usluge.service';
 })
 export class MojiZahteviComponent implements OnInit {
 
-  constructor(private us:UslugeService, protected router: Router, private fb:FormBuilder) { }
-
+  constructor(private us:UslugeService,private userS:UserService, protected router: Router, private fb:FormBuilder) { }
+  user: User = null;
   zahtev: Zahtev;
   zahtevi: Zahtev[]=[];
 
   ngOnInit(): void {
     this.getZahtevi();
+    this.getMe();
   }
 
   
@@ -28,4 +30,15 @@ export class MojiZahteviComponent implements OnInit {
       this.zahtevi = zah})
   }
 
+  
+  getMe() {
+    let token = sessionStorage.getItem('user');
+   // let me = CryptoJS.AES.decrypt(token,'2608981412').toString(CryptoJS.enc.Utf8);
+    this.userS.getMe(token).subscribe(user => {console.log(JSON.stringify(user)); 
+       this.user = user;
+    },err => {
+      console.log(err);
+      this.router.navigate(['/']);
+    })
+  }
 }
